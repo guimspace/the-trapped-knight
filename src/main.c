55a60fe0049df6bd88d21f9d98d7e16fd3a39263
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2019-02-07 15:55:50 -0200

    Build a square spiral with integers

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 
 #define BOARD_SIZE 100
 
+int init_chessboard(unsigned int chessboard[][BOARD_SIZE]);
+
 
 int main(int argc, char **argv)
 {
@@ -14,6 +16,88 @@ int main(int argc, char **argv)
 			chessboard[i][j] = 0;
 	}
 
+
+	init_chessboard(chessboard);
+
+	return 0;
+}
+
+
+int init_chessboard(unsigned int chessboard[][BOARD_SIZE])
+{
+	int dir, posX, posY;
+	int incX, incY;
+	int stepsX, stepsY;
+	int i, n, N;
+
+	n = 0;
+	N = 0;
+	stepsX = 0;
+	stepsY = 0;
+
+	dir = 0;
+	posX = (BOARD_SIZE - 1) / 2;
+	posY = (BOARD_SIZE - 1) / 2;
+
+
+	while (N < BOARD_SIZE) {
+		switch (dir) {
+			case 0:
+				incX = 1;
+				incY = 0;
+
+				stepsX++;
+				N = stepsX;
+
+				dir++;
+				break;
+
+			case 1:
+				incX = 0;
+				incY = 1;
+
+				stepsY++;
+				N = stepsY;
+
+				dir++;
+				break;
+
+			case 2:
+				incX = -1;
+				incY = 0;
+
+				stepsX++;
+				N = stepsX;
+
+				dir++;
+				break;
+
+			case 3:
+				incX = 0;
+				incY = -1;
+
+				stepsY++;
+				N = stepsY;
+
+				dir = 0;
+				break;
+
+			default:
+				printf("build_path(): Switch case is default.\n\n");
+				return 1;
+		}
+
+		i = 0;
+		while (i < N) {
+			n++;
+			chessboard[posY][posX] = n;
+
+			posX += incX;
+			posY += incY;
+
+			i++;
+		}
+	}
 
 	return 0;
 }
