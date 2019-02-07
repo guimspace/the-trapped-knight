2544fb8ec4eb0e34b5fe5d6bdcc56cb030c6c6a5
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2019-02-07 15:42:03 -0200

    Define and initialize chessboard

--- src/main.c
+++ src/main.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BOARD_SIZE 100
+
 
 int main(int argc, char **argv)
 {
+	unsigned int chessboard[BOARD_SIZE][BOARD_SIZE];
+	int i, j;
+
+	for (i = 0; i < BOARD_SIZE; i++) {
+		for (j = 0; j < BOARD_SIZE; j++)
+			chessboard[i][j] = 0;
+	}
+
 
 	return 0;
 }
