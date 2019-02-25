82fdf17dba00ce088ed6baedbc58b1be6a689a73
Guilherme Tadashi Maeoka <gui.mspace@gmail.com>
2019-02-25 09:35:13 -0300

    Rename plot_number_chessboard to plot_mod_chessboard

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
 }
 
 
-void plot_number_chessboard(unsigned int chessboard[][BOARD_SIZE])
+void plot_mod_chessboard(unsigned int chessboard[][BOARD_SIZE])
 {
 	int i, j;
 
--- src/main.h
+++ src/main.h
@@ -3,6 +3,6 @@
 
 #define BOARD_SIZE 100
 
-void plot_number_chessboard(unsigned int chessboard[][BOARD_SIZE]);
+void plot_mod_chessboard(unsigned int chessboard[][BOARD_SIZE]);
 void plot_char_chessboard(unsigned int chessboard[][BOARD_SIZE]);
 void knight_move(unsigned int chessboard[][BOARD_SIZE], int init_posX, int init_posY);
