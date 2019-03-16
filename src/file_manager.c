f020558b998567bbc5563c21c11824e49c491755
Guilherme T Maeoka <gui.mspace@gmail.com>
2019-03-15 23:42:28 -0300

    Change file extension *.dat to *.data

--- src/file_manager.c
+++ src/file_manager.c
@@ -4,21 +4,21 @@
 
 int open_files(struct str_files *files)
 {
-	files->fSequence = fopen("./Data/sequence.dat", "w");
+	files->fSequence = fopen("./Data/sequence.data", "w");
 	if (files->fSequence == NULL) {
-		printf("File sequence.dat coult not be opened.\n");
+		printf("File sequence.data coult not be opened.\n");
 		return 1;
 	}
 
-	files->fPlot = fopen("./Data/plot.dat", "w");
+	files->fPlot = fopen("./Data/plot.data", "w");
 	if (files->fPlot == NULL) {
-		printf("File plot.dat coult not be opened.\n");
+		printf("File plot.data coult not be opened.\n");
 		return 1;
 	}
 
 	files->fStats = fopen("./Data/stats.dat", "w");
 	if (files->fStats == NULL) {
-		printf("File stats.dat coult not be opened.\n");
+		printf("File stats.data coult not be opened.\n");
 		return 1;
 	}
 
